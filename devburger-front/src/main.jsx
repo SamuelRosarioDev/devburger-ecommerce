58dc5647ba5cb1f448e4e11794fdc8bca763dc7e
SamuelRosarioDev <samuelrosario.dev@gmail.com>
2025-01-31 17:30:30 -0300

    add system outlet

--- devburger-front/src/main.jsx
+++ devburger-front/src/main.jsx
@@ -1,22 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import AppProvider from "./hooks";
-import { routers } from "./routes";
 import GlobalStyles from "./styles/globalStyles";
 import { Elements } from "@stripe/react-stripe-js";
 import stripePromise from "./config/stripeConfig";
 import { ThemeProvider } from "styled-components";
 import { standardTheme } from "./styles/themes/stardard";
+import { Router } from "./routes";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<ThemeProvider theme={standardTheme}>
 			<AppProvider>
 				<Elements stripe={stripePromise}>
-					<RouterProvider router={routers} />
+					<BrowserRouter>
+						<Router />
+					</BrowserRouter>
 				</Elements>
 				<GlobalStyles />
 				<ToastContainer theme="dark" />
--- devburger-front/src/routes/index.jsx
+++ devburger-front/src/routes/index.jsx
@@ -1,31 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { Route, Routes } from "react-router-dom";
 import { Cart, Checkout, CompletePayment, Home, Login, Menu, Register } from "../pages";
+import UserLayout from "../layouts/UserLayout";
 
-export const routers = createBrowserRouter([
-	{
-		path: "/",
-		element: (
-			<>
-				<Header />
-				<Home />
-				<Footer />
-			</>
-		),
-	},
-	{ path: "/login", element: <Login /> },
-	{ path: "/cadastro", element: <Register /> },
-	{
-		path: "/cardapio",
-		element: (
-			<>
-				<Header />
-				<Menu />
-			</>
-		),
-	},
-	{ path: "/carrinho", element: <Cart /> },
-	{ path: "/checkout", element: <Checkout /> },
-	{ path: "/complete", element: <CompletePayment /> },
-]);
+
+export function Router() {
+	return (
+		<Routes>
+			<Route path="/" element={<UserLayout />} >
+				<Route path="/" element={<Home />} />
+				<Route path="/cardapio" element={<Menu />} />
+				<Route path="/carrinho" element={<Cart />} />
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/complete" element={<CompletePayment />} />
+			</Route>
+
+			<Route path="/login" element={<Login />} />
+			<Route path="/cadastro" element={<Register />} />
+
+		</Routes>
+	)
+
+}
