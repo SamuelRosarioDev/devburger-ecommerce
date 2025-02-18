abe563f296d8d4759635462df3c5e2b4045b3d42
SamuelRosarioDev <samuelrosario.dev@gmail.com>
2025-02-18 15:24:21 -0300

    updated Orders

--- devburger-front/src/pages/Admin/Orders/index.jsx
+++ devburger-front/src/pages/Admin/Orders/index.jsx
@@ -1,7 +1,61 @@
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Row } from './row';
+import { useEffect, useState } from 'react';
+import { api } from '../../../services/api';
+
+
 export function Orders() {
+    const [orders, setOrders] = useState([])
+    const [rows, setRows] = useState([])
+
+    useEffect(() => {
+        async function loadOrders() {
+            const { data } = await api.get('/orders')
+            setOrders(data)
+        }
+    }, [])
+
+    function createData(order) {
+        return {
+            orderId: order._id,
+            name: order.user.name,
+            date: order.createdAt,
+            status: order.status,
+            products: order.products,
+        };
+    }
+
+    useEffect(() => {
+        const newRows = orders.map(order => createData(order))
+        setRows(newRows)
+    }, [orders])
+
+
     return (
-        <div>
-            <h1>Orders</h1>
-        </div>
-    )
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell>Pedido</TableCell>
+                        <TableCell>Cliente</TableCell>
+                        <TableCell>Data do Pedido</TableCell>
+                        <TableCell>Status</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <Row key={row._id} row={row} />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
